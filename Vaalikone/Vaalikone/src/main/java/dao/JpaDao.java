e291061df695e9a5bc74fc5ef7d0c0f6d9301194
Teija O <tebiaz8@gmail.com>
2022-05-06 19:57:13 +0300

    Update JpaDao.java

--- Vaalikone/Vaalikone/src/main/java/dao/JpaDao.java
+++ Vaalikone/Vaalikone/src/main/java/dao/JpaDao.java
@@ -1,6 +1,13 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 
 import data.Answers;
 import data.Candidates;
@@ -28,11 +35,33 @@ public class JpaDao {
 			return new Answers();
 		}
 		
-		public void deleteAnswer(Answers answer) {
+		public ArrayList<Answers> deleteAnswer(Answers answer) {
+			em.getTransaction().begin();
+			Answers a=em.find(Answers.class, answer.getId());
+			if (a!=null) {
+				em.remove(a);//The actual insertion line
+			}
+			em.getTransaction().commit();
+			ArrayList<Answers> list=readAnswers();		
+			return list;
+		}
+		public ArrayList<Answers> updateAnswer(Answers answer) {
+			em.getTransaction().begin();
+			Answers a=em.find(Answers.class, answer.getId());
+			if (a!=null) {
+				em.merge(answer);//The actual update line
+			}
+			em.getTransaction().commit();
+			//Calling the method readFish() of this service
+			ArrayList<Answers> list=readAnswers();		
+			return list;
 			
 		}
-		public void updateAnswer(Answers answer) {
-			
+		public ArrayList<Answers> readAnswers() {
+			em.getTransaction().begin();
+			ArrayList<Answers> list=(ArrayList<Answers>) em.createQuery("select a from Answers a").getResultList();
+			em.getTransaction().commit();
+			return list;
 		}
 
 		//candidates
@@ -49,11 +78,33 @@ public class JpaDao {
 			return new Candidates();
 		}
 		
-		public void deleteCandidate(Candidates candidate) {
-			
+		public ArrayList<Candidates> deleteCandidate(Candidates candidate) {
+			em.getTransaction().begin();
+			Candidates c=em.find(Candidates.class, candidate.getId());
+			if (c!=null) {
+				em.remove(c);//The actual insertion line
+			}
+			em.getTransaction().commit();
+			ArrayList<Candidates> list=readCandidates();		
+			return list;
 		}
-		public void updateCandidate(Candidates candidate) {
-			
+		public ArrayList<Candidates> updateCandidate(Candidates candidate) {
+			em.getTransaction().begin();
+			Candidates c=em.find(Candidates.class, candidate.getId());
+			if (c!=null) {
+				em.merge(candidate);//The actual update line
+			}
+			em.getTransaction().commit();
+			//Calling the method readFish() of this service
+			ArrayList<Candidates> list=readCandidates();		
+			return list;
+		}
+		
+		public ArrayList<Candidates> readCandidates() {
+			em.getTransaction().begin();
+			ArrayList<Candidates> list=(ArrayList<Candidates>) em.createQuery("select a from Candidates a").getResultList();
+			em.getTransaction().commit();
+			return list;
 		}
 		
 		//questions
@@ -70,11 +121,33 @@ public class JpaDao {
 			return new Questions();
 		}
 		
-		public void deleteQuestion(Questions question) {
-			
+		public ArrayList<Questions> deleteQuestion(Questions question) {
+			em.getTransaction().begin();
+			Questions q=em.find(Questions.class, question.getId());
+			if (q!=null) {
+				em.remove(q);//The actual insertion line
+			}
+			em.getTransaction().commit();
+			ArrayList<Questions> list=readQuestions();		
+			return list;
 		}
-		public void updateQuestion(Questions question) {
-			
+		public ArrayList<Questions> updateQuestions(Questions question) {
+			em.getTransaction().begin();
+			Questions q=em.find(Questions.class, question.getId());
+			if (q!=null) {
+				em.merge(question);//The actual update line
+			}
+			em.getTransaction().commit();
+			//Calling the method readFish() of this service
+			ArrayList<Questions> list=readQuestions();		
+			return list;
+		}
+		
+		public ArrayList<Questions> readQuestions() {
+			em.getTransaction().begin();
+			ArrayList<Questions> list=(ArrayList<Questions>) em.createQuery("select a from Questions a").getResultList();
+			em.getTransaction().commit();
+			return list;
 		}
 		
 		//subjects - en tiedä tarvitaanko, mutta varavuoksi
@@ -91,10 +164,32 @@ public class JpaDao {
 			return new Subjects();
 		}
 		
-		public void deleteSubject(Subjects subject) {
-			
+		public ArrayList<Subjects> deleteSubjects(Subjects subject) {
+			em.getTransaction().begin();
+			Subjects s=em.find(Subjects.class, subject.getId());
+			if (s!=null) {
+				em.remove(s);//The actual insertion line
+			}
+			em.getTransaction().commit();
+			ArrayList<Subjects> list=readSubjects();		
+			return list;
 		}
-		public void updateSubject(Subjects subject) {
-			
+		public ArrayList<Subjects> updateSubjects(Subjects subject) {
+			em.getTransaction().begin();
+			Subjects s=em.find(Subjects.class, subject.getId());
+			if (s!=null) {
+				em.merge(subject);//The actual update line
+			}
+			em.getTransaction().commit();
+			//Calling the method readFish() of this service
+			ArrayList<Subjects> list=readSubjects();		
+			return list;
+		}
+		
+		public ArrayList<Subjects> readSubjects() {
+			em.getTransaction().begin();
+			ArrayList<Subjects> list=(ArrayList<Subjects>) em.createQuery("select a from Subjects a").getResultList();
+			em.getTransaction().commit();
+			return list;
 		}
 }
