ab5b03c0de775aedc9c05d2d606f7871d7b6382f
jussi20127 <102256229+jussi20127@users.noreply.github.com>
2022-05-06 15:09:00 +0300

    @Entity annotaatiot Questions ja Candidates

--- Vaalikone/Vaalikone/src/main/java/data/Questions.java
+++ Vaalikone/Vaalikone/src/main/java/data/Questions.java
@@ -1,6 +1,14 @@
 package data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Questions {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String kysymys;
 	private String aihe;
